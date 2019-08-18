e592e16cc059f24acf1b151048a7e925ef9eb2a0
SamuRule13 <54073787+SamuRule13@users.noreply.github.com>
2019-08-18 17:21:58 -0500

    Practica02

---
+++ Practica02/Sintaxis.java
@@ -0,0 +1,29 @@
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+	public static void main(String[] args) {
+		int b = 20, a = 4, c = -7;
+
+		boolean prop = true;
+		byte ochoBits = 127;
+		short dieciseisBits = -32768;
+
+		double discriminante = (b * b)-(4 * a * c);
+		double x1 = (-b + sqrt(discriminante)) / (2 * a); 
+		double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+		char ch1 = 'g';
+                char ch2 = 'w';
+		int d = 1 / (a - 3);
+
+		// Declaraciones
+
+                
+                int var_1=2147483647;
+                var_1=var_1+1; 
+                char quote = '\'';
+		
+	}
+
+}
