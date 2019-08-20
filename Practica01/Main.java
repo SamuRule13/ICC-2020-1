d811cf8101de5aaad11f7758fe64392e006e6bdf
SamuRule13 <54073787+SamuRule13@users.noreply.github.com>
2019-08-20 10:27:39 -0500

    Update Main.java

--- Practica01/Main.java
+++ Practica01/Main.java
@@ -1,8 +1,8 @@
 public class Main{
 public static void main (String []args)
 {
-System.out.println("OpenJDK");
-System.out.println("OpenJDK");
-System.out.println("OpenJDK");
+System.out.println("openjdk 11.0.4 2019-07-16");
+System.out.println("OpenJDK Runtime Environment");
+System.out.println("OpenJDK 64-Bit Server VM");
 }
 }
